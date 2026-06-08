986ed5daa7b771a2aa20eac6f74536a27950fc46
Fury1755 <robbstark1755@gmail.com>
2026-06-08 13:25:30 +0800

    test(test_download_excel): added mock test for function download_excel

--- tests/test_boundary/test_excel_io.py
+++ tests/test_boundary/test_excel_io.py
@@ -1,36 +1,55 @@
-'''This module conducts unit tests for excel_io.py'''
+'''
+This module conducts unit tests for excel_io.py.
+'''
 
 from boundary.excel_io import download_excel
 from factories.mock_response import MockAPIResponse
 from unittest.mock import patch, MagicMock
+import pytest
 
+@pytest.mark.parametrize("status, ok", [
+    ("400", False),
+    ("200", True),
+    ("304", False)
+])
+def test_download_excel(status: int, ok: bool):
+    '''
+    This function tests if download_excel can handle various responses.
+    This test is meaningful because it verifies:
+    - when ok is true, the correct workbook is returned
+    - when ok is false, RuntimeError is raised
+    - when ok is false, error message returns response status and response text
+    '''
 
-@patch('boundary.excel_io.openpyxl.load_workbook')
-@patch('boundary.excel_io.request_with_retry') # patch the local function
-# passes a MagicMock object into the parameter
-# the order it is passed into the function is from bottom-most
-#  decorator to top
-# openpyxl.load_workbook is an independent external dependency
-#  within the function. It's return value does NOT follow from mocking
-#  a request, therefore it must be mocked as well.
-def test_download_excel(mock_request, mock_workbook):
-    '''This function tests if download_excel can handle various responses.'''
+    # initialize our response according to the arguments passed
+    mock_response = MockAPIResponse(status=status, ok=ok)
 
-    mock_endpoint = "https://doesntmatter.com"
-    mock_response = MockAPIResponse(mock_endpoint, body = b"test_bytes")
-    mock_request.return_value = mock_response
+    # patch the local function
+    with patch('boundary.excel_io.request_with_retry') as mock_request:
+        mock_request.return_value = mock_response
 
-    # we are focusing on the response, so we don't need
-    #  an actual value for the excel wb.
-    fake_wb = MagicMock()
-    mock_workbook.return_value = fake_wb
-    wb = download_excel(MagicMock(), MagicMock(), mock_endpoint)
+        # openpyxl.load_workbook is an independent external dependency
+        #  within the function. It's return value does NOT follow from mocking
+        #  a request, therefore it must be mocked as well.
+        with patch('boundary.excel_io.openpyxl.load_workbook') as mock_workbook:
+            fake_wb = MagicMock()
+            mock_workbook.return_value = fake_wb
+            if not ok:
+                # assert that runtimeerror will be raised
+                with pytest.raises(RuntimeError) as exc_info:
+                    wb = download_excel(MagicMock(), MagicMock(), "raw_url doesnt matter")
+                    assert exc_info.value == (f"Download failed: {mock_response.status} - "
+                    f"{mock_response.text()}")
+                    return
+            elif ok:
+                wb = download_excel(MagicMock(), MagicMock(), "raw_url doesnt matter")
+                # assert that
+                # 1. the function ran successfully
+                # 2. it returned the exact object we returned by load_workbook
+                #     (MagicMock())
+                # 'is' asserts equality by reference (memory addresses)
+                # '==' checks value equality
+                # although '==' works here, use 'is'
+                assert wb is fake_wb
 
-    # assert that
-    # 1. the function ran successfully
-    # 2. it returned the exact object we returned by load_workbook
-    #     (MagicMock())
-    # 'is' asserts equality by reference (memory addresses)
-    # '==' checks value equality
-    # although '==' works here, use 'is'
-    assert wb is fake_wb
+                return
