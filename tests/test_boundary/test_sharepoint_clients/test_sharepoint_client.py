5b38ddeacd8f5ba6e15b205fa6f95bc273ed7fcc
Fury1755 <robbstark1755@gmail.com>
2026-06-09 17:08:07 +0800

    test: added test_create_folder

--- tests/test_boundary/test_sharepoint_clients/test_sharepoint_client.py
+++ tests/test_boundary/test_sharepoint_clients/test_sharepoint_client.py
@@ -19,6 +19,8 @@ def test_get_next_company_coarse():
     - get_next_company, when called with a mock object 'response_json',
       correctly passes the next company's name to its instantiated output.
 
+    Does not test error handling.
+
     """
 
     with (
@@ -32,6 +34,8 @@ def test_get_next_company_coarse():
         mock_best_match_item.return_value = folder_names
         mock_get_item_data.return_value = folder_names
         mock_decide_folder.return_value = "mock ServerRelativeUrl"
+
+        # simulates the json structure of what sharepoint returns
         mock_response_json = {
             "d": {
                 "Files": {"results": [{"Name": "file1"}, {"Name": "file2"}]},
@@ -57,3 +61,83 @@ def test_get_next_company_coarse():
         dummy.get_next_company(folder_names[0])
 
         assert mock_build_query.call_args.args[0] == folder_names[1]
+
+
+def test_create_folder():
+    """
+    This test verifies that:
+    - correct arguments are passed to get_request_digest
+    - correct upload endpoint is constructed
+    - response is correctly unwrapped (implicit in the example json)
+    - request arguments are correctly passed
+
+    Does not test error handling.
+    """
+
+    with (
+        patch("boundary.response_helpers.get_request_digest") as mock_request_digest,
+        patch.object(SharePointClient, "_walk_folder") as mock_walk_folder,
+        patch.object(SharePointClient, "request") as mock_request,
+        patch.object(SharePointClient, "_build_client_query") as mock_build_query,
+    ):
+        mock_digest = "fake digest"
+        names = [
+            "SIR GET DOWN",
+            "SIR GET DOWN NOW",
+            "Ayyy, ayyy",
+            "ayyy",
+            "to the windowww",
+        ]
+        mock_response_json = {
+            "Files": {
+                "results": [
+                    {"Name": names[0]},
+                    {"Name": names[1]},
+                    {"Name": names[2]},
+                ]
+            },
+            "Folders": {
+                "results": [{"Name": names[3]}, {"Name": names[4]}],
+            },
+        }
+        mock_request_digest.return_value = mock_digest
+        mock_walk_folder.return_value = MockAPIResponse(
+            "doesnt matter", mock_response_json
+        )
+
+        mock_site_url = "fake_site_url"
+        mock_relative_url = "fake_relative_url"
+        # keep the urls as actual strings; we need them to verify the endpoint.
+        mock_page = MagicMock()
+        dummy = SharePointClient(
+            mock_page,
+            mock_site_url,
+            mock_relative_url,
+            MagicMock(),
+            MagicMock(),
+            MagicMock(),
+        )
+
+        mock_folder_name = "fake_folder_name"
+        fake_endpoint = (
+            f"{mock_site_url}/_api/web/GetFolderByServerRelativeUrl"
+            f"('{mock_relative_url}')"
+            f"/Folders/add('{mock_folder_name}')"
+        )
+
+        dummy.create_folder(mock_folder_name)
+
+        request_positional_args = (
+            "POST",
+            fake_endpoint,
+        )
+        header_value = {
+            "Accept": "application/json;odata=verbose",
+            "X-RequestDigest": mock_digest,
+        }
+
+        mock_request_digest.assert_called_once_with(mock_page, mock_site_url)
+        mock_request.assert_called_once_with(
+            *request_positional_args, headers=header_value
+        )
+        mock_build_query.assert_called_once_with(mock_folder_name)
