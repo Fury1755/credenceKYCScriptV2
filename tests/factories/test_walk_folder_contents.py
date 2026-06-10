feea42039f9e3f471f3083b9d82ba6920e499ce9
Fury1755 <robbstark1755@gmail.com>
2026-06-10 14:11:29 +0800

    test: created simple behavioural test for the folder factory

---
+++ tests/factories/test_walk_folder_contents.py
@@ -0,0 +1,25 @@
+"""
+This module tests the create_folder function in walk_folder_contents.
+Personally, it clarifies the behaviour of create_folder for me, and is inexpensive
+to write.
+"""
+
+from walk_folder_contents import create_folder
+
+
+def test_create_folder():
+    """
+    A quick, implementation based test of create_folder.
+    """
+    properties_list = (str("united investments"), str("random url"), str("random time"))
+    result = create_folder([properties_list])
+
+    assert "Folders" in result
+    assert "results" in result["Folders"]
+
+    first = result["Folders"]["results"][0]
+    print(result)
+    print(first)
+    assert "united investments" in first["Name"]
+    assert "random url" in first["ServerRelativeUrl"]
+    assert "random time" in first["TimeLastModified"]
